19bc2c8d5843175740877cf8eda9051bf91c124b
175247 <yarr-tm@hotmail.com>
2020-11-26 13:47:59 +0100

    Add initial Fizzbuzz.

--- CleanCodeLabb1/Program.cs
+++ CleanCodeLabb1/Program.cs
@@ -2,11 +2,30 @@
 
 namespace CleanCodeLabb1
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+        }
+
+        public string Fizzbuzz()
+        {
+            for (int i = 0; i < 300; i++)
+            {
+                if (i == 42)
+                    Console.WriteLine("Answer to the Ultiamte Question of Life, the Universe, and Everything");
+
+                else if (i % 3 == 0 && i % 5 == 0)
+                    Console.WriteLine("Fizzbuzz");
+
+                else if (i % 3 == 0)
+                    Console.WriteLine("Fizz");
+
+                else if (i % 5 == 0)
+                    Console.WriteLine("Buzz");
+            }
+            return null;
         }
     }
 }

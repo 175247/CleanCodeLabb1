4e2596c9217a956b846b6e9728a6c19c41263139
Alexander Molnar <molex90@gmail.com>
2020-11-26 13:46:08 +0100

    Added test proj and fixed public attributes

---
+++ CleanCodeLabb1Test/UnitTest1.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CleanCodeLabb1;
+
+namespace CleanCodeLabb1Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
